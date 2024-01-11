e22c87c4f6686ffcfb0c7149ef789c58fcf79302
Uttiya WinHome <uttiyaghosh@gmail.com>
2024-01-11 14:13:01 -0500

    Deleted all express contents

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:current-alpine3.19
-WORKDIR /app/
-COPY package*.json /app/
-RUN npm install
-COPY . .
-EXPOSE 3000
-ENTRYPOINT ["npm","start"]
--- src/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const portfolioRouter = require("./portfolio")
-
-const router = express.Router()
-
-router.use("/",portfolioRouter)
-
-module.exports = router
