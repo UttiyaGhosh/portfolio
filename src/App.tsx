f0e4f01d4e5d9b0789303993c0c83f21966cd072
Uttiya WinHome <uttiyaghosh@gmail.com>
2024-01-18 15:37:21 -0500

    Updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {HashRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route, BrowserRouter} from 'react-router-dom'
 import Home from "./pages/Home"
 import Contact from "./pages/Contact"
 import './App.css'
@@ -24,12 +24,12 @@ function App() {
   // );
   
   return (
-    <HashRouter>
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element = {<Home />} />
         <Route path="/contact" element = {<Contact />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
