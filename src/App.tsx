3bee85777b78ce709ec32b9ae7e593f3c2ccb4d8
Uttiya WinHome <uttiyaghosh@gmail.com>
2024-01-18 15:09:36 -0500

    Updated to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {HashRouter,Routes,Route} from 'react-router-dom'
 import Home from "./pages/Home"
 import Contact from "./pages/Contact"
 import './App.css'
@@ -24,12 +24,12 @@ function App() {
   // );
   
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element = {<Home />} />
         <Route path="/contact" element = {<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
