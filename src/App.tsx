24d37dc5cb1600f217a995e3c251c9386f50da89
Uttiya WinHome <uttiyaghosh@gmail.com>
2024-01-19 12:03:44 -0500

    Updated Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,10 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from "./pages/Home"
-import Contact from "./pages/Contact"
 import './App.css'
 
 function App() {
-
-  // const Home = () => (
-  //   <div>
-  //     <h2>Home</h2>
-  //   </div>
-  // );
-  
-  // const About = () => (
-  //   <div>
-  //     <h2>About</h2>
-  //   </div>
-  // );
-  
-  // const Contact = () => (
-  //   <div>
-  //     <h2>Contact</h2>
-  //   </div>
-  // );
   
   return (
-    <BrowserRouter basename="/">
-      <Routes>
-        <Route path="/" element = {<Home />} />
-        <Route path="/contact" element = {<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <Home/>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home"
+import Contact from "./pages/Contact"
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import App from './App.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/vite-react-router/",
+    element: <App />,
+    children: [
+      {
+        path: "/vite-react-router/",
+        element: <Home />,
+      },
+      {
+        path: "/vite-react-router/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
